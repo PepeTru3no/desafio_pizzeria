6ce8a634a1ee39d9862c496fec5febd6cac7c1dd
PepeTru3no <jvinetconsola@gmail.com>
2024-11-13 22:04:37 -0300

    esta si es la buena
    carga de primera version de desafio pizzeria

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Nav_bar from './components/Nav_bar'
+import Home from './components/Home'
+import Footer from './components/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav_bar/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
